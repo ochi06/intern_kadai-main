1ba3e7311aaf733d4347878ac6708b63087fd2f1
ochi06 <sd23652@st.omu.ac.jp>
2025-10-17 13:09:51 +0900

    Users,rojectsテーブル作成

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -8,7 +8,7 @@ return array(
 		'type'        => 'mysqli',
 		'connection'  => array(
 			'hostname'   => 'db',
-			'database'   => 'fuelphp',
+			'database'   => 'todo_schedule',
 			'username'   => 'root',
 			'password'   => 'root',
 			'persistent' => false,
---
+++ fuel/app/config/development/migrations.php
@@ -0,0 +1,22 @@
+<?php
+return array(
+  'version' => 
+  array(
+    'app' => 
+    array(
+      'default' => 
+      array(
+        0 => '00000000000001_create_users',
+        1 => '00000000000002_create_projects',
+      ),
+    ),
+    'module' => 
+    array(
+    ),
+    'package' => 
+    array(
+    ),
+  ),
+  'folder' => 'migrations/',
+  'table' => 'migration',
+);
