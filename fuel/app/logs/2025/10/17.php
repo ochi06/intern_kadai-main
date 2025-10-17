46005a6e8ee7028a12e3897fb84f4e7fcc7691d9
ochi06 <sd23652@st.omu.ac.jp>
2025-10-17 11:18:56 +0900

    環境構築

---
+++ fuel/app/logs/2025/10/17.php
@@ -0,0 +1,4 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2025-10-17 11:13:14 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-10-17 11:18:20 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
